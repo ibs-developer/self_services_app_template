4241df3bac73105d075a567717a228d3d5838ad9
Usama Al Iraqy <ibs-developer>
2025-08-18 13:56:49 +0300

    small fix for avatar

--- src/components/reusable/avatar.tsx
+++ src/components/reusable/avatar.tsx
@@ -5,7 +5,7 @@ import { Image, View } from "react-native";
 const Avatar: FC<IAvatar> = ({ className, ...props }) => {
   return (
     <View
-      className={`h-[100] aspect-square bg-blue-100 rounded-full ${className}`}
+      className={`h-[100] aspect-square bg-blue-100 rounded-full overflow-hidden ${className}`}
     >
       <Image style={{ width: "100%", height: "100%" }} {...props} />
     </View>
